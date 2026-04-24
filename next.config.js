ef8bc75ad2af5954a06bb526ac2f408677df68da
Jason Aballe <jasonaballe0@gmail.com>
2026-04-24 21:56:30 +0800

    new updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/portfolio-jason-aballe', 
+  assetPrefix: '/portfolio-jason-aballe',
   images: {
-    unoptimized: true, 
+    unoptimized: true,
   },
 }
 
